3acfffba72343cf7ec4d22c0307927d1f779f6c3
Mayur <nilayagokula@gmail.com>
2026-05-03 18:47:35 +0530

    added pop_back function

--- vector.c
+++ vector.c
@@ -1,41 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct{
+typedef struct {
     int *data;
     int capacity;
     int size;
 } Vector;
 
-void init(Vector* v){
-    v->capacity=2;
-    v->size=0;
-    v->data=(int*)malloc(v->capacity*sizeof(int));
-    if(v->data==NULL){
+void init(Vector* v) {
+    v->capacity = 2;
+    v->size = 0;
+    v->data = (int*)malloc(v->capacity*sizeof(int));
+    if(v->data == NULL) {
         printf("\nMemory allocation failed\n");
         exit(1);
             }
         }
 
-void push_back(Vector* v, int value){
-    if(v->size==v->capacity){
+void push_back(Vector* v, int value) {
+    if(v->size == v->capacity) {
         int new_capacity=v->capacity*2;
-        int *temp=(int*)realloc(v->data, new_capacity*sizeof(int));
-        if(temp!=NULL){
-            v->data=temp;
-            v->capacity=new_capacity;
-        }
-        else{
+        int *temp = (int*)realloc(v->data, new_capacity*sizeof(int));
+        if(temp == NULL) {
             printf("\nMemory Re-allocation failed\n");
             exit(1);
         }
+        else if(temp != NULL){             
+            v->data = temp;
+            v->capacity = new_capacity;
+        }
     } 
-    
-        v->data[v->size]=value;
+        v->data[v->size] = value;
         v->size++;
 }
-void pop_back(Vector* v){ //uses lazy deletion
+
+void pop_back(Vector* v) {//uses lazy deletion
+
+    if(v->size == 0){
+        printf("Vector is empty\n");
+        exit(1);}
+    v->size--;
     
+    if(v->size < (int)v->capacity/4) {    //commonly used threshold
+        int new_capacity;
+        if (v->capacity>1) new_capacity = v->capacity/2;    
+        int *temp = (int*)realloc(v->data,new_capacity*sizeof(int));
+        if(temp == NULL) {
+            printf("\nMemory Re-allocation failed\n");
+            exit(1);
+        }
+        else if(temp != NULL) {
+            v->data = temp;
+            v->capacity = new_capacity;
+        }
+    }
 }
+
                                                                                                           
     
